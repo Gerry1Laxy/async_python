e5c7d5782069a9a32276e51a3c509eb374874d12
Gerry1Laxy <mr.john.milton@yandex.ru>
2023-03-09 12:55:20 +0300

    completed async python hw

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from models import SwapiPeople, engine, Session, Base
 
 
 CHUNK_SIZE = 10
-LAST_PEOPLE = 50
+LAST_PEOPLE = 83
 
 rewrited_filds = {
     'films': 'title',
@@ -16,48 +16,46 @@ rewrited_filds = {
     'starships': 'name',
     'vehicles': 'name'
 }
+deleted_filds = (
+    'created',
+    'edited',
+    'url'
+)
 
-# hash_req = dict()
+hash_req = dict()
 
-async def get_people(
+async def get_item(
         ses: Session, people_id: int = None, subqueries: str = None
     ):
     if subqueries:
-        # chek_hash = hash_req.get(subqueries)
-        # if chek_hash:
-        #     print('hash')
-        #     print(chek_hash)
-        #     return chek_hash
+        chek_hash = hash_req.get(subqueries)
+        if chek_hash:
+            return chek_hash
         url = subqueries
     else:
         url = f'https://swapi.dev/api/people/{people_id}'
     async with ses.get(url) as response:
         response_json = await response.json()
-        if people_id:
-            await rewrite_item(ses, response_json)
-            # for field, name in rewrited_filds.items():
-            #     items = await asyncio.gather(
-            #         *(get_people(ses, subqueries=subq) for subq in response_json[field])
-            #     )
-            #     response_json[field] = ', '.join([item[name] for item in items])
-            # films = await asyncio.gather(*(get_people(ses, subqueries=subq) for subq in response_json['films']))
-            # print(', '.join([film['title'] for film in films]))
-        # else:
-        #     hash_req[subqueries] = response_json
+        if people_id and response.status == 200:
+            response_json['id'] = people_id
+            await rewrite_fields(ses, response_json)
+            await delete_fields(response_json)
+        else:
+            hash_req[subqueries] = response_json
         return response_json
 
-async def rewrite_item(ses: Session, people):
-    homeworld = await get_people(ses, subqueries=people['homeworld'])
-    # print(homeworld['name'])
+async def rewrite_fields(ses: Session, people):
+    homeworld = await get_item(ses, subqueries=people['homeworld'])
     people['homeworld'] = homeworld['name']
     for field, name in rewrited_filds.items():
         items = await asyncio.gather(
-            *(get_people(ses, subqueries=subq) for subq in people[field])
+            *(get_item(ses, subqueries=subq) for subq in people[field])
         )
         people[field] = ', '.join([item[name] for item in items])
-    # for people in peoples:
-    #     films = await asyncio.gather(*(get_people(ses, subqueries=subq) for subq in people['films']))
-    #     print(', '.join([film['title'] for film in films]))
+
+async def delete_fields(people):
+    for field in deleted_filds:
+        people.pop(field)
 
 async def paste_to_db(items):
     items = [SwapiPeople(json=item) for item in items]
@@ -72,15 +70,17 @@ async def main():
         await conn.run_sync(Base.metadata.create_all)
 
     session = aiohttp.ClientSession()
-    coroutins = (get_people(session, i) for i in range(1, LAST_PEOPLE))
+    coroutins = (get_item(session, i) for i in range(1, LAST_PEOPLE))
     for chunk in chunked(coroutins, CHUNK_SIZE):
         results = await asyncio.gather(*chunk)
         asyncio.create_task(paste_to_db(results))
-        break
+        # break
     await session.close()
     for task in asyncio.all_tasks():
         if task != asyncio.current_task():
             await task
+    
+    print(results)
 
     print(datetime.now() - start)
 
